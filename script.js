0c45cb64e0b503b1b2f4d9393b9ae71e3ff329a9
Munish <hmunish5@gmail.com>
2023-07-16 22:37:53 +0200

    Add sum arrow function & Student object

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello world");
+
+const sum = (a, b) => a + b;
+console.log(sum(5, 7)); // returns 12
+
+const Student = {
+  fullName: "Munish",
+  age: 25,
+  greet() {
+    console.log(`Hi, my name is ${this.fullName}`);
+  },
+};
+
+Student.greet();
