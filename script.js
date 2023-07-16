18a313f62dc4f86faef3d4d39dd057a0ee508161
Munish <hmunish5@gmail.com>
2023-07-16 23:38:09 +0200

    Add server config with node.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const http = require("http");
+
 console.log("Hello world");
 
 const sum = (a, b) => a + b;
@@ -12,3 +14,21 @@ const Student = {
 };
 
 Student.greet();
+
+let fruits = ["apple", "oranges", " ", "mango", " ", "lemon"];
+fruits = fruits.map((e) => {
+  if (e === " ") return "empty string";
+  else return e;
+});
+
+console.log(fruits);
+
+// Start server
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html><body><h1>Hello from the server</h1></body></html>");
+  res.end();
+});
+
+server.listen(3000);
