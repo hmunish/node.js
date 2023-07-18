c6fb65f4cc6bf3efb2ed1aa03301863a7cf9aa55
Munish <hmunish5@gmail.com>
2023-07-18 20:18:19 +0200

    Add routes for server

--- script.js
+++ script.js
@@ -26,9 +26,26 @@ console.log(fruits);
 // Start server
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html><body><h1>Hello from the server</h1></body></html>");
-  res.end();
+  const url = req.url;
+  if (url === "/home") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html><body><h1>Welcome home</h1></body></html>");
+    return res.end();
+  } else if (url === "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html><body><h1>Welcome to About us page</h1></body></html>");
+    return res.end();
+  } else if (url === "/node") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      "<html><body><h1>Welcome to my node js project</h1></body></html>"
+    );
+    return res.end();
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html><body><h1>Default</h1></body></html>");
+    return res.end();
+  }
 });
 
 server.listen(3000);
