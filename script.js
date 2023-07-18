7ef42338484a06cbdeb0d8fafaebac6c72c68079
Munish <hmunish5@gmail.com>
2023-07-18 22:25:47 +0200

    Add message display feature

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 console.log("Hello world");
 
@@ -27,6 +28,7 @@ console.log(fruits);
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === "/home") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html><body><h1>Welcome home</h1></body></html>");
@@ -41,9 +43,26 @@ const server = http.createServer((req, res) => {
       "<html><body><h1>Welcome to my node js project</h1></body></html>"
     );
     return res.end();
+  } else if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
   } else {
+    const message = fs.readFileSync("message.txt");
     res.setHeader("Content-Type", "text/html");
-    res.write("<html><body><h1>Default</h1></body></html>");
+    res.write(
+      `<html><body><form method='POST' action='/message'><h3>${message}</h3><input type='text' name='message'><button type='submit'>Submit</button></form></body></html>`
+    );
     return res.end();
   }
 });
