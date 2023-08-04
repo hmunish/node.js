6c8e019d5d37d17be72f79dc5a163174ad1139db
Munish <hmunish5@gmail.com>
2023-08-04 15:11:59 +0200

    Add add to cart form

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 const http = require("http");
 // const routes = require("./routes");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/products" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add product</button></form>'
+  );
+});
+
+app.post("/products", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use((req, res, next) => {
-  console.log("In another middleware");
   res.send("<h1>Hello from Express.js</h1>");
 });
 
 // Start server
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
